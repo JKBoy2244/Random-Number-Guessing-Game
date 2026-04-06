6c8396270d08a19a5a5260009c79bdc4d24c878c
JKBoy2244 <jobayer2006@gmail.com>
2026-04-06 22:22:50 +0100

    Create Intro.py

---
+++ src/Intro.py
@@ -0,0 +1,7 @@
+class Intro:
+
+  def welcome(self):
+
+    print("Welcome to the Random Number Generator Guessing Game!")
+    time.sleep(2)
+    print("We hope you will enjoy this game no matter what happens!")
