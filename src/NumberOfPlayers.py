10acac8f8e32081d9bc7058f97683cbba1e3048c
JKBoy2244 <jobayer2006@gmail.com>
2026-04-06 22:23:55 +0100

    Create NumberOfPlayers.py

---
+++ src/NumberOfPlayers.py
@@ -0,0 +1,65 @@
+import random
+import time
+import re
+import sys
+
+class NumberOfPlayers:
+
+  def __init__(self, number):
+    self.number = number
+
+  def get_number(self) -> int:
+    return self.number
+
+  def set_number(self, number: int):
+    self.number = number
+
+  def Input(self):
+
+    maxAttempts = 10
+    attempts = 0
+
+    while (attempts != maxAttempts):
+
+        print("Minimum number of players is 4 and maximum number of players is 9: ")
+        numberPlayers = input("How many players are playing the random number generator game?")
+        attempts+=1
+
+        if (not numberPlayers.strip()):
+
+          if attempts == maxAttempts:
+            sys.exit("Too many attempts.")
+
+          print("Empty value isn't allowed, please try again!")
+          print("You now have", maxAttempts-attempts, "left now!")
+          continue
+
+        else:
+
+          try: 
+
+            numberOfPlayer = int(numberPlayers)
+
+            if not (4 <= numberOfPlayer <= 9):
+
+              if attempts == maxAttempts:
+                sys.exit("Too many attempts.")
+
+              print("That's less than the minimum number of players. Please try again")
+              print("You now have", maxAttempts-attempts, "left now!")
+              continue
+
+            else:
+
+              self.set_number(numberOfPlayer)
+              print("You successfully entered the correct number of players")
+              return 
+
+          except ValueError:
+
+            if attempts == maxAttempts:
+              sys.exit("Too many attempts.")
+
+            print("That's invalid input. Please try again")
+            print("You now have", maxAttempts-attempts, "left now!")
+            continue
