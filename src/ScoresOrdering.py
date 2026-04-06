3c2aab488f2b2b74dd70c13cbd05a42861660d9a
JKBoy2244 <jobayer2006@gmail.com>
2026-04-06 22:25:45 +0100

    Create ScoresOrdering.py

---
+++ src/ScoresOrdering.py
@@ -0,0 +1,13 @@
+class ScoresOrdering:
+
+  def left_set(self, index, randomNumbersList):
+
+    left_set = 2 * index + 1
+    print(left_set)
+    return left_set
+
+  def right_set(self, index, randomNumbersList):
+
+    right_set = 2 * index + 2
+    print(right_set)
+    return right_set
