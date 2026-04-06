132c122a0811c0ff49e3fd22c0f5359da130a655
JKBoy2244 <jobayer2006@gmail.com>
2026-04-06 23:24:16 +0100

    Create test_winners.py

---
+++ tests/test_winners.py
@@ -0,0 +1,14 @@
+import pytest
+from Winner import Winner
+
+class TestWinner:
+
+  def testWinner(self):
+
+    winner = Winner()
+    playerList = ["ABCDE@45671b", "ABCDE@45671d","ABCDE@45671f","ABCDE@45671h","ABCDE@45671k", "ABCDE@45671o"] 
+    scoreList = [0, 140, 120, 190, 200, 40]
+    result = winner.displayWinner(playerList, scoreList)
+
+    assert max(scoreList) == 200
+    assert result == "ABCDE@45671k"
