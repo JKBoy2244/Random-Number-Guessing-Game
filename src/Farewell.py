58beef5ada460a35f3ad9871bf86e79e3508f033
JKBoy2244 <jobayer2006@gmail.com>
2026-04-06 22:26:33 +0100

    Create Farewell.py

---
+++ src/Farewell.py
@@ -0,0 +1,7 @@
+class Farewell:
+
+  def Farewell(self):
+
+    print("Thank you for playing this game. We hope you enjoyed it!")
+    time.sleep(2)
+    print("We hope you will play again sometime. Bye bye and take care")
