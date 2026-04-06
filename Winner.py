a751a06a9c4c5909014ae188e442e9b1881d91d8
JKBoy2244 <jobayer2006@gmail.com>
2026-04-06 23:57:54 +0100

    Update Winner.py

--- Winner.py
+++ Winner.py
@@ -2,6 +2,9 @@ class Winner:
 
   def displayWinner(self, playerScoreList, playerList):
 
+    if playerScoreList and isinstance(playerScoreList[0], str):
+        playerScoreList, playerList = playerList, playerScoreList
+      
     highestScore = playerScoreList[0]
     winnerIndex = 0
     for i in range(len(playerScoreList)):
@@ -12,3 +15,4 @@ class Winner:
 
     print("The highest score was", highestScore)
     print("The winner of the game is", playerList[winnerIndex])
+    return playerList[winnerIndex]
