514fc3fe671c0a088f1b948befc642a4bd63f743
JKBoy2244 <jobayer2006@gmail.com>
2026-04-07 00:02:05 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,7 @@ import os
 import sys
 
 ROOT = os.path.dirname(os.path.dirname(__file__))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
+SRC = os.path.join(ROOT, "src")
+
+if SRC not in sys.path:
+    sys.path.insert(0, SRC)
